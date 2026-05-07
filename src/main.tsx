a2fa755ea9f214bcf41a00a7beedd3a952d7a2fd
Bartosz V. Bentkowski <vforge@users.noreply.github.com>
2026-05-07 07:48:43 -0500

    do not use service worker on dev

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,10 @@ createRoot(document.getElementById('root')!).render(
   </StrictMode>,
 )
 
-if ('serviceWorker' in navigator) {
+const isLocalhost =
+  window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+
+if ('serviceWorker' in navigator && import.meta.env.PROD && !isLocalhost) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch(() => {
       // ignore registration failure
